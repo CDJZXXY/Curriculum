9549c8948acde1e1074ad51ac7d75b946556cde6
CDJZXXY <daisy_benihime@foxmail.com>
2024-10-17 23:06:38 +0900

    fix: ts_test.sh

--- algorithm/typescript/question1/ts_test.sh
+++ algorithm/typescript/question1/ts_test.sh
@@ -1,24 +1,21 @@
-# #!/bin/zsh
+#!/bin/zsh
 
-# date "+%Y/%m/%d %H:%M"
+date "+%Y/%m/%d %H:%M"
 
-# # コーディングルールをチェック
-# npm run lint
+# コーディングルールをチェック
+npm run lint
 
-# # テストパターン1から5の出力をチェック
-# for i in 1 2 3 4 5
-# do
-#   cat ./input/input${i} | ../node_modules/.bin/ts-node index.ts > ./output
+# テストパターン1から5の出力をチェック
+for i in 1 2 3 4 5
+do
+  cat ./input/input${i} | ../node_modules/.bin/ts-node index.ts > ./output
 
-#   # diffで差分がなければOKを表示
-#   RESULT=$(diff -q ./expected/expected${i} ./output);
-#   if [ "RESULT" = "" ]; then
-#       echo "テストパターン${i}: OK！"
-#   else
-#       echo "テストパターン${i}: NO！"
-#   fi
-# done
-# rm ./output
-
-cat ./input/input1 | ../node_modules/.bin/ts-node index.ts > ./output
-diff -q ./expected/expected1 ./output
+  # diffで差分がなければOKを表示
+  RESULT=$(diff -q ./expected/expected${i} ./output);
+  if [ "$RESULT" = "" ]; then
+      echo "テストパターン${i}: OK！"
+  else
+      echo "テストパターン${i}: NO！"
+  fi
+done
+rm ./output
